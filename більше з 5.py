974c4ed57bacd04f943c15bb2486f55f73adab3d
GKovtonyuk <112175748+GKovtonyuk@users.noreply.github.com>
2023-03-31 11:00:49 +0300

    Add files via upload

---
+++ більше з 5.py
@@ -0,0 +1,18 @@
+a = 5
+b = 9
+c = 2
+d = 7
+e = 4
+
+largest = a  # припустимо, що найбільше число - перше
+
+if b > largest:
+    largest = b
+if c > largest:
+    largest = c
+if d > largest:
+    largest = d
+if e > largest:
+    largest = e
+
+print(largest)
